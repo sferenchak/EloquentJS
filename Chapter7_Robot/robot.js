a6967ee72192250c3c5c68e604126d468234eae6
Steve Ferenchak <steveferenchak@gmail.com>
2019-04-23 12:09:00 -0700

    Chapter 7 complete

--- Chapter7_Robot/robot.js
+++ Chapter7_Robot/robot.js
@@ -81,13 +81,6 @@ let first = new VillageState('Post Office', [
 ]);
 let next = first.move("Alice's House");
 
-console.log(next.place);
-// → Alice's House
-console.log(next.parcels);
-// → []
-console.log(first.place);
-// → Post Office
-
 // A delivery robot looks at the world and decides in which direction it
 // wants to move. As such, we could say that a robot is a function that
 // takes a VillageState object and returns the name of a nearby place.
@@ -108,7 +101,6 @@ function runRobot(state, robot, memory) {
 		let action = robot(state, memory);
 		state = state.move(action.direction);
 		memory = action.memory;
-		console.log(`Moved to ${action.direction}`);
 	}
 }
 
@@ -131,7 +123,7 @@ function randomRobot(state) {
 	return { direction: randomPick(roadGraph[state.place]) };
 }
 
-runRobot(VillageState.random(), randomRobot);
+//runRobot(VillageState.random(), randomRobot);
 
 // a route that passes all places in the village
 const mailRoute = [
@@ -158,9 +150,9 @@ function routeRobot(state, memory) {
 	return { direction: memory[0], memory: memory.slice(1) };
 }
 
-runRobot(VillageState.random(), routeRobot, []);
+//runRobot(VillageState.random(), routeRobot, []);
 
-// implement a function to determine shortest routes
+// implement a function to determine routes
 function findRoute(graph, from, to) {
 	let work = [{ at: from, route: [] }];
 	for (let i = 0; i < work.length; i++) {
@@ -186,4 +178,81 @@ function goalOrientedRobot({ place, parcels }, route) {
 	return { direction: route[0], memory: route.slice(1) };
 }
 
-runRobot(VillageState.random(), goalOrientedRobot, []);
+//runRobot(VillageState.random(), goalOrientedRobot, []);
+
+// ****Measuring a robot ****
+// It’s hard to objectively compare robots by just letting them solve a few
+// scenarios. Maybe one robot just happened to get easier tasks or the kind
+// of tasks that it is good at, whereas the other didn’t.
+
+// Write a function compareRobots that takes two robots (and their starting
+// memory). It should generate 100 tasks and let each of the robots solve each
+// of these tasks. When done, it should output the average number of steps
+// each robot took per task.
+
+// For the sake of fairness, make sure you give each task to both robots,
+// rather than generating different tasks per robot.
+
+// function to count the number of steps a robot takes to complete a task
+function countSteps(state, robot, memory) {
+	for (let steps = 0; ; steps++) {
+		if (state.parcels.length == 0) return steps;
+		let action = robot(state, memory);
+		state = state.move(action.direction);
+		memory = action.memory;
+	}
+}
+
+function compareRobots(robot1, memory1, robot2, memory2) {
+	let total1 = 0;
+	total2 = 0;
+	for (let i = 0; i < 100; i++) {
+		let state = VillageState.random();
+		total1 += countSteps(state, robot1, memory1);
+		total2 += countSteps(state, robot2, memory2);
+	}
+	console.log(`${robot1.name} needed ${total1 / 100} steps per task`);
+	console.log(`${robot2.name} needed ${total2 / 100}`);
+}
+
+// ****Robot efficiency****
+// Can you write a robot that finishes the delivery task faster than
+// goalOrientedRobot? If you observe that robot’s behavior, what obviously
+// stupid things does it do? How could those be improved?
+
+// If you solved the previous exercise, you might want to use your
+// compareRobots function to verify whether you improved the robot.
+
+function myRobot({ place, parcels }, route) {
+	if (route.length == 0) {
+		// describe a route for every parcel
+		let routes = parcels.map(parcel => {
+			if (parcel.place != place) {
+				return {
+					route: findRoute(roadGraph, place, parcel.place),
+					pickUp: true
+				};
+			} else {
+				return {
+					route: findRoute(roadGraph, place, parcel.address),
+					pickUp: false
+				};
+			}
+		});
+		console.log(routes);
+		// determine the precedence a route gets when choosing.
+		// Route length counts negatively, routes that pick up a package
+		// get a small bonus.
+		function score({ route, pickUp }) {
+			return (pickUp ? 0.5 : 0) - route.length;
+		}
+		route = routes.reduce((acc, current) => {
+			return score(acc) > score(current) ? acc : current;
+		}).route;
+	}
+	return { direction: route[0], memory: route.slice(1) };
+}
+
+runRobot(VillageState.random(), myRobot, []);
+
+//compareRobots(myRobot, [], goalOrientedRobot, []);
